9cf9d1c5a5069cc92ad9306150e65e2dd257fe88
ccpro <157919290@qq.com>
2023-04-05 15:50:42 +0800

    1

--- preload.js
+++ preload.js
@@ -4,13 +4,13 @@ window.handle = function () {
 	// const path = __dirname
 	// utools.showNotification(path);
 	// const cod = `python ${path}/demo.py`
-	const cod = `python D:\demo.py`
+	const cod = `python D:\demo.py`          // 直接写死目录算了，执行D盘的demo.py
 	// utools.showNotification(cod);
 	// utools.copyText(cod);
 	const info = execSync(cod)
-	const str = info.toString();
-	utools.showNotification(str);
-	if (str.length != 4) {
+	const str = info.toString();  // info是串数字，转换为字符串才能知道是什么
+	utools.showNotification(str);  // 正常输出OK，这里显示的就是OK
+	if (str.length != 4) {         // 长度不等于4，也就是不是OK，就有问题，就把输出的问题放到剪贴板中。
 		utools.copyText(str);
 	}
 }
